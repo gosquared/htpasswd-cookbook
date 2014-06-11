73fe77b19f3cf4ffd1bd468a53e0e0a386d35b1a
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-06-11 16:02:29 -0400

    Add name to metadata
    To be able to use it with Berkshelf 3 :
    
    While Chef does not strictly enforce this requirement, Ridley cannot continue without a valid metadata 'name' entry

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email  "gerhard@lazu.co.uk"
 license           "Apache 2.0"
 description       "A generic htpasswd implementation leveraging openssl directly"
 version           "0.2.0"
+name              "htpasswd"
 
 recipe "htpasswd", "Sets up a common htpasswd directory, uses openssl directly"
 
